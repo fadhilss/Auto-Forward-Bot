1dafe2eed7dd639af0ff1f453694b9c54a2ab9bf
fadhilss <89438065+fadhilss@users.noreply.github.com>
2021-10-17 13:27:18 +0800

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -6,15 +6,15 @@ logger = logging.getLogger(__name__)
 import asyncio
 from pyrogram import filters
 from bot import channelforward
-from config import *
+from config import Config
 
 @channelforward.on_message(filters.channel)
 async def forward(c, m):
     # Forwarding the messages to the channel
 
-    for id in CHANNEL:
-       from_channel, to_group = id.split(":")
+    for id in Config.CHANNEL:
+       from_channel, to_channel = id.split(":")
        if m.chat.id == int(from_channel):
-          await m.forward(int(to_group), as_copy=True)
+          await m.forward(int(to_channel), as_copy=True)
           print("Forwarded a message from", from_channel, "to", to_channel)
           asyncio.sleep(1)
