a2daa3915878682c7f8fb250f345ae6abc465ffe
fadhilss <89438065+fadhilss@users.noreply.github.com>
2021-10-17 18:28:31 +0800

    Update __init__.py

--- Plugins/database/__init__.py
+++ Plugins/database/__init__.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from config import DATABASE_URL
+from config import DB_URI
 
 
 def start() -> scoped_session:
-    engine = create_engine(DATABASE_URL)
+    engine = create_engine(DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
