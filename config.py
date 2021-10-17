c3561b9d5f79f078c0f9981e0867d09ca3fb1731
fadhilss <89438065+fadhilss@users.noreply.github.com>
2021-10-17 18:03:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,11 @@ class Config(object):
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
+      DATABASE_URL = os.environ.get('DATABASE_URL', None)
+      DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql") 
+      try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
