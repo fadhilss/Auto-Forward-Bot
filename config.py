c81c1028680a8815cede59cccb47b09495becab5
fadhilss <89438065+fadhilss@users.noreply.github.com>
2021-10-17 18:26:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ class Config(object):
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
-      DATABASE_URL = os.environ.get('DATABASE_URL', None)
-      DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql") 
+      DB_URI = os.environ.get("DATABASE_URL", "")
       SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
