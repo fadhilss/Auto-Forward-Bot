874710aa373d46b69e3d1f7f3499237f7ae83160
fadhilss <89438065+fadhilss@users.noreply.github.com>
2021-10-17 18:57:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,4 @@ class Config(object):
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       CHANNEL = list(x for x in os.environ.get("CHANNEL_ID", "").replace("\n", " ").split(' '))
-      DB_URI = os.environ.get("DATABASE_URL", "")
-      ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(x))
+      
